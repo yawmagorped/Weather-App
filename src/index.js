3a04063c30b5d6bc1516d5dcb6cbd8e3743991da
ali <193356044+yawmagorped@users.noreply.github.com>
2025-12-07 18:58:19 +0330

    add weather forecast for the week

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "./style.css";
 
-function getWeatherInfo(cityName, apiKey) {
+import {
+    getDay,
+} from "date-fns";
+
+
+
+function getWeatherInfo(cityName) {
     return new Promise( (resolove) => { 
-        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=${apiKey}`)
+        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=KVD85U3K57WFPHZ3B9XDQ3G38`)
         .then(function(response) {
             return response.json();
         })
@@ -11,7 +17,7 @@ function getWeatherInfo(cityName, apiKey) {
             resolove(response);
         });
     });
-};
+}
 
 let input = document.querySelector(".search > input");
 let button = document.querySelector(".search > button");
@@ -19,20 +25,67 @@ button.addEventListener('click', () => {
     displayWeatherInfo(input.value);
 });
 
+function weekdayString(dayOfTheWeek) {
+    switch (dayOfTheWeek) {
+        case 0:
+            return "Sunday";
+            break;
+        case 1:
+            return "Monday";
+            break;
+        case 2:
+            return "Tuesday";
+            break;
+        case 3:
+            return "Wednesday";
+            break;
+        case 4:
+            return "Thursday";
+            break;
+        case 5:
+            return "Friday";
+            break;
+        case 6:
+            return "Saturday";
+            break;
+    }
+}
+
 async function displayWeatherInfo(cityName) {
     let display = document.querySelector(".body-left");
     
-    let weatherInfo = await getWeatherInfo(cityName, "KVD85U3K57WFPHZ3B9XDQ3G38");
-    
+    let weatherInfo = await getWeatherInfo(cityName);
+
     let conditions = display.querySelector(".w-conditions");
     let temp = display.querySelector(".w-temp");
     let description = display.querySelector(".w-description");
     let sunrise = display.querySelector(".w-sunrise");
     let sunset = display.querySelector(".w-sunset");
 
+    let titles = display.querySelectorAll(".title");
+    let descriptions = display.querySelectorAll(".description");
+
+    console.log(titles);
+    console.log(descriptions);
+
+    
     conditions.textContent = weatherInfo.currentConditions.conditions;
     temp.textContent = `${weatherInfo.currentConditions.temp}F (feels like:${weatherInfo.currentConditions.feelslike}F)`;
     description.textContent = weatherInfo.description;
     sunrise.textContent = `sunrise: ${weatherInfo.currentConditions.sunrise}`;
     sunset.textContent = `sunset: ${weatherInfo.currentConditions.sunset}`;
+    
+    let today = getDay(weatherInfo.days[0].datetime);
+    let counter = today;
+    titles.forEach(title => {
+        counter++;
+        title.textContent = weekdayString(counter);
+    });
+    counter = today;
+
+    descriptions.forEach(description => {
+        description.textContent = weatherInfo.days[counter].conditions;
+        counter++;
+    });
 }
+
