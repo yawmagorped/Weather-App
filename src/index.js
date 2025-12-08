388ba7cc8c0df087364f6a5bcdd449bf01d94534
ali <193356044+yawmagorped@users.noreply.github.com>
2025-12-08 18:04:12 +0330

    add loading indicator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import "./style.css";
-
+import loading from "./images/loading_gray.gif";
 import {
     getDay,
 } from "date-fns";
 
 
+let input = document.querySelector(".search > input");
+let button = document.querySelector(".search > button");
+button.addEventListener('click', () => {
+    displayWeatherInfo(input.value);
+});
+
+input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter')
+        displayWeatherInfo(input.value);
+});
 
 function getWeatherInfo(cityName) {
     return new Promise( (resolove) => { 
+        loadingDisplay(1);
         fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=KVD85U3K57WFPHZ3B9XDQ3G38&unitGroup=metric`)
         .then(function(response) {
             return response.json();
@@ -15,14 +26,24 @@ function getWeatherInfo(cityName) {
         .then(function(response) {
             console.log(response);
             resolove(response);
+        })
+        .catch(function(reject) {
+            loadingDisplay(0);
+            console.error(reject);
         });
     });
 }
 
-let input = document.querySelector(".search > input");
-let button = document.querySelector(".search > button");
-button.addEventListener('click', () => {
-    displayWeatherInfo(input.value);
+const loadingDisplay =( (toggle) => {
+    if (toggle) {
+        let img = document.createElement("img");
+        img.src = loading;
+        button.after(img);
+    }
+    else {
+        let img = button.nextElementSibling;
+        img.remove();
+    }
 });
 
 function weekdayString(dayOfTheWeek) {
@@ -54,8 +75,11 @@ function weekdayString(dayOfTheWeek) {
 async function displayWeatherInfo(cityName) {
     let display = document.querySelector(".body-left");
     
+    //check for valid cityName
+
     let weatherInfo = await getWeatherInfo(cityName);
 
+    let address = display.querySelector(".w-cityName");
     let conditions = display.querySelector(".w-conditions");
     let temp = display.querySelector(".w-temp");
     let description = display.querySelector(".w-description");
@@ -68,7 +92,7 @@ async function displayWeatherInfo(cityName) {
     console.log(titles);
     console.log(descriptions);
 
-    
+    address.textContent = weatherInfo.resolvedAddress;
     conditions.textContent = weatherInfo.currentConditions.conditions;
     temp.textContent = `${weatherInfo.currentConditions.temp}C (feels like:${weatherInfo.currentConditions.feelslike}C)`;
     description.textContent = weatherInfo.description;
@@ -95,4 +119,7 @@ async function displayWeatherInfo(cityName) {
         })
         description.append(img);
     });
+    loadingDisplay(0);
 }
+
+displayWeatherInfo("Tehran");
